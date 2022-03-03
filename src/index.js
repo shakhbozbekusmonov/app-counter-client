685dca9e2ac099df3d4ba658e80dd483eefbe576
shakhbozbekusmonov <miracle_0319@bk.ru>
2022-03-03 10:37:06 +0500

    Created Redux-Counter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {rootReducer} from "./redux/reducers/rootReducer";
+import Counter from "./components/Counter";
 
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Counter/>
+  </Provider>,
   document.getElementById('root')
 );
